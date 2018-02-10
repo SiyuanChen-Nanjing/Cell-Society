47a94b4249dcf863ba301800bf96bad8b7dc09e6
Jamie <nk143@duke.edu>
2018-02-10 18:37:54 -0500

    added polygon features not complete

--- src/cells/Cell.java
+++ src/cells/Cell.java
@@ -2,6 +2,12 @@ package cells;
 
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Polygon;
+import grids.Grid;
+import grids.GridModel;
+import grids.HexagonGrid;
+import grids.RectangleGrid;
+import grids.TriangleGrid;
 
 public abstract class Cell {
 	
@@ -23,9 +29,31 @@ public abstract class Cell {
 	private int myGridX;
 	private int myGridY;
 
+	private Grid myGrid;
+	private Polygon myShape;
+	private GridModel myGridModel;
+	
+	
 	public Cell(double x_pos, double y_pos, double width, double height) {
+		myGrid = new RectangleGrid();
 		myRectangle = new Rectangle(x_pos, y_pos, width, height);
 		myRectangle.setStroke(Color.BLACK);
+	}
+	
+	public void setGridModel (Grid grid) {
+		myGrid = grid;
+		if (grid instanceof RectangleGrid) {
+			myGrid = new RectangleGrid();
+		} else if (grid instanceof TriangleGrid) {
+			myGrid = new TriangleGrid();
+		} else if (grid instanceof HexagonGrid) {
+			myGrid = new HexagonGrid();
+		}
+		createCells();
+	}
+	
+	public void createCells() {
+		
 	}
 	
 	public Cell(double x_pos, double y_pos, double width, double height, int i, int j) {
@@ -34,6 +62,8 @@ public abstract class Cell {
 		myGridX = i;
 		myGridY = j;
 	}
+	
+	
 
 	public int getMyGridX() {
 		return myGridX;
@@ -62,6 +92,10 @@ public abstract class Cell {
 	public Rectangle getMyRectangle() {
 		return myRectangle;
 	}
+	
+	public GridModel getMyGrid(){
+		return myGridModel;
+	}
 
 	public boolean isEmpty() {
 		return isEmpty;
@@ -81,6 +115,14 @@ public abstract class Cell {
 	
 	public boolean isTree() {
 		return isTree;
+	}
+
+	public Polygon getMyShape() {
+		return myShape;
+	}
+
+	public void setMyShape(Polygon myShape) {
+		this.myShape = myShape;
 	}
 
 }
